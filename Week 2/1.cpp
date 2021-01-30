5111ed90dad6e8415912c5af4b339e1e9e4326af
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-30 10:20:42 +0530

    Initial Commit

--- Week 2/1.cpp
+++ Week 2/1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <list>
+#include <map>
 using namespace std;
 
 class Sparse{
@@ -20,7 +21,8 @@ public:
 };
 
 int main(){
-    list<Sparse> l1;
+//    list<Sparse> l1;
+    map<pair<int,int>,int> m1;
     cout<<"Enter Row and Column of 1st Sparse Matrix:\n";
     int row1,col1;
     cin>>row1>>col1;
@@ -31,34 +33,44 @@ int main(){
     for (int i = 0; i < n1; ++i) {
         int r,c,d;
         cin>>r>>c>>d;
-        Sparse sparse(r,c,d);
-        l1.push_back(sparse);
+        m1.insert(make_pair(make_pair(r,c),d));
     }
 
-    list<Sparse> l2;
-    cout<<"Enter Row and Column of 1st Sparse Matrix:\n";
+//    list<Sparse> l2;
+    map<pair<int,int>,int> m2;
+    cout<<"Enter Row and Column of 2nd Sparse Matrix:\n";
     int row2,col2;
     cin>>row2>>col2;
 
-    cout<<"Enter number of entries of 1st Sparse Matrix:\n";
+    cout<<"Enter number of entries of 2nd Sparse Matrix:\n";
     int n2;
     cin>>n2;
     for (int i = 0; i < n2; ++i) {
         int r,c,d;
         cin>>r>>c>>d;
-        Sparse sparse(r,c,d);
-        l2.push_back(sparse);
+        m2.insert(make_pair(make_pair(r,c),d));
     }
-    list<Sparse> l3;
+    map<pair<int,int>,int > output;
     if (col1==row2){
-        for(auto it=l1.begin();it!=l1.end();it++){
-            int r = it->row;
-            for(auto it2=l2.begin();it2!=l2.end();it2++){
-                int c = it->col;
-                Sparse sparse(r,c,0);
-                l3.push_back(sparse);
+        for(auto it=m1.begin();it!=m1.end();it++){
+            int r = (it->first).first;
+            for(auto it2=m2.begin();it2!=m2.end();it2++){
+                int c = (it2->first).second;
+                output[make_pair(r,c)] = 0;
             }
         }
+        for(auto it=m1.begin();it!=m1.end();it++){
+            int r = (it->first).first;
+            for(auto it2=m2.begin();it2!=m2.end();it2++){
+                int c = (it2->first).second;
+                output[make_pair(r,c)] += it->second * it2->second;
+                cout<<r<<" "<<c<<" "<<output[make_pair(r,c)]<<'\n';
+            }
+        }
+        for(auto it=output.begin();it!=output.end();it++){
+            cout<<(it->first).first<<" "<<(it->first).second<<" "<<it->second<<"\n";
+        }
+
     } else{
         cout<<"Multiplication Not Possible.\n";
     }

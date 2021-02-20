e56e456d7e23a981d0dd1c3d26dcbaf8c6e001dd
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-20 10:15:45 +0530

    Initial Commit

--- Week 5/1.cpp
+++ Week 5/1.cpp
@@ -38,10 +38,30 @@ bool search(Trie *root,string key){
     }
     return temp!= nullptr && temp->isEndOfWord;
 }
+void removeUtil(Trie *root,string key,int i){
+    if (i!=key.length()){
+        int index = key[i] - 'a';
+        removeUtil(root->children[index],key,i+1);
+        delete root;
+    }
+}
 
 void remove(Trie *root,string key){
     Trie *temp = root;
-
+    Trie *lastEndOfWord = root;
+    int lastEndOfWordIndex = 0;
+    for (int i = 0; i < key.length(); ++i) {
+        int index = key[i] - 'a';
+        if (!temp->children[index]){
+            cout<<"Element Not Found";
+        }
+        if (i!=key.length()-1 && temp->isEndOfWord){
+            lastEndOfWord = temp;
+            lastEndOfWordIndex = i;
+        }
+        temp = temp->children[index];
+    }
+    removeUtil(lastEndOfWord,key,lastEndOfWordIndex);
 }
 
 int main(){
@@ -58,6 +78,9 @@ int main(){
 
     cout<<"Enter Word To Be Searched :\n";
     cin>>str;
-    cout<<(search(root,str)?"Word Exists":"Word Does Not Exists");
+    cout<<(search(root,str)?"Word Exists":"Word Does Not Exists")<<"\n";
 
+    cout<<"Enter Word To Be Deleted :\n";
+    cin>>str;
+    remove(root,str);
 }

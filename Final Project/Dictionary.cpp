951ca2d5bf65e5bd7a8d3d103a965cd7dfb59881
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-03-01 01:20:11 +0530

    Initial Commit

--- Final Project/Dictionary.cpp
+++ Final Project/Dictionary.cpp
@@ -78,13 +78,72 @@ bool remove(Trie *root,string key){
     removeUtil(root,key,0,found);
     return found;
 }
+
+void displayOutput(vector<string> values){
+    int i=1;
+    cout<<yellow;
+    for (auto it = values.begin(); it != values.end(); it++) {
+        cout<<i<<". "<< *it << "\n";
+        i++;
+    }
+}
+void getAllRecommendationUtil(Trie *root,string key,vector<string> &v){
+    if (root) {
+        if (root->isEndOfWord) {
+            v.push_back(key);
+        }
+        for (auto it=root->children.begin();it!=root->children.end();it++) {
+            getAllRecommendationUtil(it->second, key + it->first, v);
+        }
+    }
+}
+vector<string> getAllRecommendation(Trie *root,string key){
+    Trie *temp = root;
+    bool found = false;
+    for (int i = 0; i < key.length(); ++i) {
+        int index = key[i];
+        if (!temp->children[index]){
+            cout<<"Element Not Found";
+            found = true;
+            break;
+        }
+        temp = temp->children[index];
+    }
+    vector<string> v;
+    if (!found) {
+        getAllRecommendationUtil(temp, key, v);
+    }
+    return v;
+}
+void getWordOfTheDay(Trie *root){
+    Trie *temp = root;
+    string str = "";
+    int i;
+    char index='a';
+    srand(time(0));
+    while (!temp->isEndOfWord){
+        int numberOfChildren = temp->children.size();
+        i = rand()%numberOfChildren;
+        int k = 0;
+        for(auto it=temp->children.begin();it!=temp->children.end();it++){
+            if(k==i){
+                index = it->first;
+                break;
+            }
+            k++;
+        }
+        str += index;
+        temp = temp->children[index];
+    }
+    cout<<yellow<<"Word Of The Day is : "<<green<<"\""<<str<<"\""<<'\n';
+    displayOutput(temp->data);
+}
 string toLowerCase(string str){
     transform(str.begin(), str.end(), str.begin(), ::tolower);
     return str;
 }
 json readJsonFile(){
     json j;
-
     ifstream readJSON("C://Users/jgoel/CLionProjects/Non-Linear-Data-Structure-And-Algorithm/Final project/data.json");
     j = json::parse(readJSON);
     return j;
@@ -101,6 +160,7 @@ void displayChoice(){
     cout<<blue<<"1--->Search A Word\n";
     cout<<blue<<"2--->Delete A Word\n";
     cout<<blue<<"3--->Word Of The Day\n";
+    cout<<blue<<"4--->Recommendations Of A Word\n";
     cout<<"\n";
 }
 void welcomeBanner(){
@@ -131,12 +191,7 @@ int main(){
 
             vector<string> values = search(root, toLowerCase(key));
             cout<<"\n";
-            int i=1;
-            cout<<yellow;
-            for (auto it = values.begin(); it != values.end(); it++) {
-                cout<<i<<". "<< *it << "\n";
-                i++;
-            }
+            displayOutput(values);
         }
         else if(ch==2){
             cout<<"Enter Word To Be Deleted :\n";
@@ -150,36 +205,18 @@ int main(){
             } else{
                 cout<<red<<"Element Not Found";
             }
-        }else if(ch==3){
-            Trie *temp = root;
-            string str = "";
-            int i;
-            char index='a';
-            vector<string> data;
-            srand(time(0));
-            while (!temp->isEndOfWord){
-                int numberOfChildren = temp->children.size();
-                i = rand()%numberOfChildren;
-                int k = 0;
-                for(auto it=temp->children.begin();it!=temp->children.end();it++){
-                    if(k==i){
-                        index = it->first;
-                        break;
-                    }
-                    k++;
-                }
-                str += index;
-                temp = temp->children[index];
-            }
-            cout<<yellow<<"Word Of The Day is : "<<green<<"\""<<str<<"\""<<'\n';
-            i=1;
-            cout<<yellow;
-            for (auto it = temp->data.begin(); it != temp->data.end(); it++) {
-                cout<<i<<". "<< *it << "\n";
-                i++;
-            }
+        }
+        else if(ch==3){
+            getWordOfTheDay(root);
+        }else if(ch==4){
+            cout << "Enter A Word :\n";
+            string key;
+            cin.ignore();
+            getline(cin,key);
+            vector<string> v = getAllRecommendation(root,key);
+            displayOutput(v);
         }else{
-            cout<<red<<"Wrong Input\n";
+            cout<<red<<"Wrong Input\nTry Again....";
             break;
         }
         char cont;
